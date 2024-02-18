eed950a759ce3258200327605ca4430cc8ce4a17
gmk0 <mkgeorges63@gmail.com>
2024-02-18 14:33:59 +0000

    save

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -65,8 +65,10 @@ class UserResource extends Resource
                 Tables\Columns\TextColumn::make('phone_verified_at')
                     ->dateTime()
                     ->sortable(),
-            Tables\Columns\IconColumn::make('referrals.user.profile_photo_path')
-                ->boolean()->toggleable(isToggledHiddenByDefault: true),
+
+            Tables\Columns\TextColumn::make('referrals_count')->label('Parainé')
+            ->counts('referrals')
+                ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\IconColumn::make('is_online')
                     ->boolean()->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('last_activity')
